ac20566bf848fd68dbd59356ff297676532f5ce0
Penryn <1515805213@163.com>
2024-05-15 01:41:25 +0800

    fix(controller):获取日志信息可以设置数量和级别

--- app/controllers/adminController/log.go
+++ app/controllers/adminController/log.go
@@ -9,19 +9,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type LogData struct {
+	Num     int `form:"num" json:"num"`
+	LogType int `form:"log_type" binding:"required,oneof=0 1 2 3 4"`
+}
+
 func GetLogMsg(c *gin.Context){
+	var data LogData
+	err := c.ShouldBindQuery(&data)
+	if err != nil {
+		c.Error(&gin.Error{Err: err, Type: gin.ErrorTypeBind})
+		utils.JsonErrorResponse(c, apiException.ParamError)
+		return
+	}
 	//鉴权
-	_, err := sessionService.GetUserSession(c)
+	_, err = sessionService.GetUserSession(c)
 	if err != nil {
 		c.Error(&gin.Error{Err: err, Type: gin.ErrorTypePublic})
 		utils.JsonErrorResponse(c, apiException.NotLogin)
 		return
 	}
-	data, err := adminService.GetLastLinesFromLogFile("app.log", 5)
+	response, err := adminService.GetLastLinesFromLogFile("app.log", data.Num, data.LogType)
 	if err != nil {
 		c.Error(&gin.Error{Err: err, Type: gin.ErrorTypePublic})
 		utils.JsonErrorResponse(c, apiException.ServerError)
 		return
 	}
-	utils.JsonSuccessResponse(c, data)
+	utils.JsonSuccessResponse(c, response)
 }
